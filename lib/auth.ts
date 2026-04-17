68dae0256cacccad2721ec93ba22688141d172db
angelcruz07 <angelcruzsanchez126@gmail.com>
2026-04-16 19:40:32 -0600

    feat: update dependencies for better-auth and add Prisma client files for user authentication

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,12 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "@/lib/prisma";
+import { stripe } from "@better-auth/stripe"
+import Stripe from "stripe"
+
+const stripeClient = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+    apiVersion: "2026-03-25.dahlia", // Latest API version as of Stripe SDK v22.0.0
+})
 
 export const auth = betterAuth({
   secret: process.env.BETTER_AUTH_SECRET,
@@ -11,4 +17,11 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  plugins: [
+    stripe({
+        stripeClient,
+        stripeWebhookSecret: process.env.STRIPE_WEBHOOK_SECRET!,
+        createCustomerOnSignUp: true,
+    })
+]
 });
