980188aac91bfb1953998f5ccedea9cd941a8e6f
angelcruz07 <angelcruzsanchez126@gmail.com>
2026-04-28 22:43:47 -0600

    chore: update dependencies in package.json and lock files, including better-auth packages to version 1.6.9, and enhance build script to include prisma generation

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,12 +1,6 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "@/lib/prisma";
-import { stripe } from "@better-auth/stripe"
-import Stripe from "stripe"
-
-const stripeClient = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-    apiVersion: "2026-03-25.dahlia", // Latest API version as of Stripe SDK v22.0.0
-})
 
 export const auth = betterAuth({
   secret: process.env.BETTER_AUTH_SECRET,
@@ -17,11 +11,4 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
-  plugins: [
-    stripe({
-        stripeClient,
-        stripeWebhookSecret: process.env.STRIPE_WEBHOOK_SECRET!,
-        createCustomerOnSignUp: true,
-    })
-]
 });
